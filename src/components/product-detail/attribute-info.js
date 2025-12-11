1ab717b774cc307b31fcac3b2ebd4d8924e5cfbc
Jochen Schneider <jochen.schneider@commercetools.com>
2025-12-11 14:18:44 +0100

    Fix color string rendering in detail view

--- src/components/product-detail/attribute-info.js
+++ src/components/product-detail/attribute-info.js
@@ -3,13 +3,14 @@ import config from '../../config';
 const VERBOSE = false;
 
 const AttributeInfo = ({attr}) => {
-  
+
   const name = attr.name;
 
   let value = JSON.stringify(attr.value,null,'\t');
   VERBOSE && console.log('attr value type', typeof attr.value);
   if(typeof attr.value === 'object' && attr.value.label !== undefined && typeof attr.value.label === 'object') {
-    value = attr.value.label[config.locale];
+    // Try config.locale first, then 'en', then first available key
+    value = attr.value.label[config.locale] || attr.value.label['en'] || Object.values(attr.value.label)[0];
   }
   if(typeof attr.value == 'boolean') {
     value = attr.value ? 'true' : 'false';
